3a70d28735601c1d1f85756e6de70e6003d1fc9b
AhmedEhab5006 <ahm.ehab1510@gmail.com>
2025-05-13 18:34:36 +0300

    ss

--- ClinicManagementSystem.API/Controllers/AdminController.cs
+++ ClinicManagementSystem.API/Controllers/AdminController.cs
@@ -212,10 +212,10 @@ namespace ClinicManagementSystem.API.Controllers
 
                 _context.Patients.Update(patient);
 
-                var foundAppointment = _context.DoctorAppointments.Where(a => a.appointmentStart == reservation.appointment.appointmentStart)
+                var foundAppointment = _context.Reservations.Where(a => a.appointment.appointmentStart == reservation.appointment.appointmentStart)
                                                                    .FirstOrDefault();
-                foundAppointment.status = "Booked";
-                _context.Update(foundAppointment);
+                foundAppointment.status = "Reserved";
+                _context.Reservations.Update(foundAppointment);
 
                 if (_context.SaveChanges() > 0)
                     return Ok();
